c7fb42550176f533d75b653e995cdb2ba4b713f8
AlexanderMurie <37141745+AlexanderMurie@users.noreply.github.com>
2018-08-15 19:33:07 +0200

    Add files via upload

---
+++ dbh.inc.php
@@ -0,0 +1,17 @@
+<!-- 
+
+Alexander Murie
+12/08/2018
+Eagleweb
+
+ -->
+
+
+<?php 
+
+$dbServername = "localhost"; /* Get UCT servername*/ 
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "Eagleweblogin";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
